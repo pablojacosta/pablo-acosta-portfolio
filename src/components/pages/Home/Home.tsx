b063844f8d85027ba79eac1f38d5d2bd2a0a7a92
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-09-18 00:09:37 -0300

    feat: :sparkles: Working on Home section.

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -4,6 +4,8 @@ import Icons from "@components/shared/Icons";
 import IconCode from "@components/elements/Icons/CodeIcon/CodeIcon";
 import Link from "next/link";
 import { PORTFOLIO_URL } from "constants/externalLinks";
+import Image from "next/image";
+import photo from "@images/profile-photo-no-bg.png";
 
 const Home = () => {
   return (
@@ -12,32 +14,48 @@ const Home = () => {
         <div className={styles.top}>
           <h1>My Portfolio</h1>
           <div className={styles.content}>
-            <div className={`${styles.square} ${styles.border} ${styles.left}`}>
-              <p>
-                Hi! My name is Pablo and I&apos;m a Full Stack Developer from
-                Argentina.
-              </p>
+            <div className={styles.leftSection}>
+              <div className={styles.text}>
+                <div
+                  className={`${styles.square} ${styles.border} ${styles.left}`}
+                >
+                  <p>
+                    Hi! My name is Pablo and I&apos;m a Full Stack Developer
+                    from Argentina.
+                  </p>
+                </div>
+                <div
+                  className={`${styles.square} ${styles.border} ${styles.right}`}
+                >
+                  <p>
+                    I discovered my passion for development after several years
+                    of playing, recording & mixing music. Now, music is my hobby
+                    and development my career.
+                  </p>
+                </div>
+                <div
+                  className={`${styles.square} ${styles.border} ${styles.left}`}
+                >
+                  <p>
+                    One day, I decided to escape from Buenos Aires city, so now
+                    I work remotely surrounded by beautiful mountains.
+                  </p>
+                </div>
+                <div className={`${styles.square} ${styles.right}`}>
+                  <p>
+                    The technologies that I use are Typescript/Javascript,
+                    React.js, Next.js, Node.js, SASS/CSS & HTML.
+                  </p>
+                </div>
+              </div>
             </div>
-            <div
-              className={`${styles.square} ${styles.border} ${styles.right}`}
-            >
-              <p>
-                I discovered my passion for development after several years of
-                playing, recording & mixing music. Now, music is my hobby and
-                development my career.
-              </p>
-            </div>
-            <div className={`${styles.square} ${styles.border} ${styles.left}`}>
-              <p>
-                One day, I decided to escape from Buenos Aires city, so now I
-                work remotely surrounded by beautiful mountains.
-              </p>
-            </div>
-            <div className={`${styles.square} ${styles.right}`}>
-              <p>
-                The technologies that I use are Typescript/Javascript, React.js,
-                Next.js, Node.js, SASS/CSS & HTML.
-              </p>
+            <div className={styles.rightSection}>
+              <Image
+                src={photo.src}
+                alt="Portfolio Photo"
+                width={250}
+                height={250}
+              />
             </div>
           </div>
         </div>
