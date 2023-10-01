3945d8b89f72b8030f2cada1dfa0aac237efd6fe
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-09-30 23:27:33 -0300

    feat: :lipstick: Small styles modification.

--- src/components/pages/Projects/components/ProjectsSlider/components/ProjectDetails/components/DetailsUsedTech/DetailsUsedTech.tsx
+++ src/components/pages/Projects/components/ProjectsSlider/components/ProjectDetails/components/DetailsUsedTech/DetailsUsedTech.tsx
@@ -1,11 +1,16 @@
-import { HTMLAttributes } from "react";
 import styles from "./DetailsUsedTech.module.scss";
+import Image from "next/image";
+import zustand from "@images/zustand.png";
+import useMediaQuery from "@hooks/useMediaQuery";
 
 interface IDetailsUsedTech {
   tech: any[];
 }
 
 const DetailsUsedTech = ({ tech }: IDetailsUsedTech) => {
+  const isMobileBreakpoint = useMediaQuery(370);
+  const zustandHeight = !isMobileBreakpoint ? 20 : 15;
+
   return (
     <div className={styles.detailsUsedTech}>
       <h2>Tech used:</h2>
@@ -17,6 +22,14 @@ const DetailsUsedTech = ({ tech }: IDetailsUsedTech) => {
             </li>
           );
         })}
+        <li className={styles.zustand}>
+          <Image
+            src={zustand.src}
+            alt="Zustand Icon"
+            width={30}
+            height={zustandHeight}
+          />
+        </li>
       </ul>
     </div>
   );
