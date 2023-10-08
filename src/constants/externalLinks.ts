02a1133c28bbba2eebc0b8517f8207820408be0b
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-08 12:12:50 -0300

    feat: :sparkles: Changed og image url.

--- src/constants/externalLinks.ts
+++ src/constants/externalLinks.ts
@@ -13,5 +13,4 @@ export const NPM_PACKAGE_CODE =
 export const LYRICS_AND_TABS = "https://lyrics-and-tabs-finder.onrender.com/";
 export const LYRICS_AND_TABS_CODE =
   "https://github.com/pablojacosta/lyrics-tabs-app-front";
-export const OG_IMAGE =
-  "https://photos.google.com/u/1/photo/AF1QipM0FiML3wXjSKPLpdYI_gUz0fWWFzoQJnj4iaQB";
+export const OG_IMAGE = "https://photos.app.goo.gl/hkCza38QSvfFtCqL7";
