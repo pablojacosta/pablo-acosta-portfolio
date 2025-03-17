a690a8084b52742cd2d643f8a76d9c350636f379
pablojacosta <pablojacosta82@gmail.com>
2025-03-17 16:32:02 -0300

    fix: update Next.js and Netlify configuration for static export

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
